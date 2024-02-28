20a585f5ac9d703cb121b55366a1543912d9d4c4
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-29 00:50:20 +0300

    init app

---
+++ database/migrations/2024_02_28_205855_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+             $table->string('title');
+             $table->string('slug')->unique();
+             $table->date('start_date');
+            $table->date('end_date');
+             $table->string('address');
+              $table->integer('num_tickets');
+              $table->foreignId('location_id')->constarined();
+              $table->string('image');
+              $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+              $table->longText('description');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
