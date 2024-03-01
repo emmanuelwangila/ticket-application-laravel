cfce001657758f2b7ac905a13612aeeead81f3ca
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-01 15:51:58 +0300

    fine tuning the User Interface

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,6 +1,14 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
+export default function InputLabel({
+    value,
+    className = "",
+    children,
+    ...props
+}) {
     return (
-        <label {...props} className={`block font-medium text-sm text-gray-700 ` + className}>
+        <label
+            {...props}
+            className={`block  text-sm text-blue-500 font-bold  ` + className}
+        >
             {value ? value : children}
         </label>
     );
