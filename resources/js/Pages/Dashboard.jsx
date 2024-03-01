16deafddaa18059b7e418283da5a3f986516e6a2
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-03-01 13:14:34 +0300

    refactor dashboard User Interface

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import NavLink from "@/Components/NavLink";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 
@@ -20,97 +21,102 @@ export default function Dashboard({ auth, events, users }) {
                     </h2>
                     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                         {/* Card 1: Total Users */}
-                        <div className="bg-white overflow-hidden shadow-sm rounded-lg">
-                            <div className="p-6">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 bg-blue-500 rounded-md p-3">
-                                        <svg
-                                            className="h-6 w-6 text-white"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                                            ></path>
-                                        </svg>
-                                    </div>
-                                    <div className="ml-4">
-                                        <p className="text-lg leading-6 font-medium text-gray-900">
-                                            Events
-                                        </p>
-                                        <p className="mt-2 text-sm text-gray-500">
-                                            5000
-                                        </p>
+                        <NavLink
+                            href={route("events")}
+                            active={route().current("events")}
+                        >
+                            <div className="bg-white overflow-hidden shadow-sm rounded-lg">
+                                <div className="p-6 bg-white">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0 bg-blue-500 rounded-md p-3">
+                                            <svg
+                                                className="h-6 w-6 text-white"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 bg-white">
+                                            <p className="text-lg leading-6 font-medium text-gray-900">
+                                                Events
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </NavLink>
 
                         {/* Card 2: Total Revenue */}
-                        <div className="bg-white overflow-hidden shadow-sm rounded-lg">
-                            <div className="p-6">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 bg-green-500 rounded-md p-3">
-                                        <svg
-                                            className="h-6 w-6 text-white"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M20 12H4"
-                                            ></path>
-                                        </svg>
-                                    </div>
-                                    <div className="ml-4">
-                                        <p className="text-lg leading-6 font-medium text-gray-900">
-                                            Users
-                                        </p>
-                                        <p className="mt-2 text-sm text-gray-500">
-                                            $50,000
-                                        </p>
+                        <NavLink
+                            href={route("users")}
+                            active={route().current("users")}
+                        >
+                            <div className="bg-white overflow-hidden shadow-sm rounded-lg">
+                                <div className="p-6 bg-white">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0 bg-yellow-500 rounded-md p-3">
+                                            <svg
+                                                className="h-6 w-6 text-white"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 bg-white">
+                                            <p className="text-lg leading-6 font-medium text-gray-900">
+                                                Users
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </NavLink>
 
-                        {/* Card 3: Recent Orders */}
-                        <div className="bg-white overflow-hidden shadow-sm rounded-lg">
-                            <div className="p-6">
-                                <div className="flex items-center">
-                                    <div className="flex-shrink-0 bg-yellow-500 rounded-md p-3">
-                                        <svg
-                                            className="h-6 w-6 text-white"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            stroke="currentColor"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                strokeWidth="2"
-                                                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                                            ></path>
-                                        </svg>
-                                    </div>
-                                    <div className="ml-4">
-                                        <p className="text-lg leading-6 font-medium text-gray-900">
-                                            Reservations
-                                        </p>
-                                        <p className="mt-2 text-sm text-gray-500">
-                                            10 new orders
-                                        </p>
+                        <NavLink
+                            href={route("reservations")}
+                            active={route().current("reservations")}
+                        >
+                            <div className="bg-white overflow-hidden shadow-sm rounded-lg">
+                                <div className="p-6 bg-white">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0 bg-green-500 rounded-md p-3">
+                                            <svg
+                                                className="h-6 w-6 text-white"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                stroke="currentColor"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                                                ></path>
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 bg-white">
+                                            <p className="text-lg leading-6 font-medium text-gray-900">
+                                                Reservations
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </NavLink>
                     </div>
                 </div>
             </div>
--- resources/js/Pages/Reservation.jsx
+++ resources/js/Pages/Reservation.jsx
@@ -6,7 +6,7 @@ import { Inertia } from "@inertiajs/inertia";
 
 import React from "react";
 
-function Reservation({ auth }) {
+function Reservation({ auth, reservations }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -22,11 +22,83 @@ function Reservation({ auth }) {
                 className="text-white font-bold m-3 "
             >
                 <div className="bg-blue-500 rounded-md w-[60%] h-[20%] text-white m-2 p-3 ">
-                    Create a Reservation
+                    Book a Reservation
                 </div>
             </NavLink>
 
-            <div>Reservations</div>
+            <div className="text-teal-600 font-bold ">Reservations</div>
+            <table className="min-w-full leading-normal">
+                <thead>
+                    <tr>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"></th>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Ticket Type
+                        </th>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"></th>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Number of Tickets
+                        </th>
+                        <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {reservations.map((reservation) => (
+                        <tr key={reservation.id}>
+                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0 w-10 h-10"></div>
+                                    <div className="ml-3">
+                                        <p className="text-blue-500 whitespace-no-wrap"></p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <p className="text-blue-500 whitespace-no-wrap">
+                                    {reservation.type}
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                    {/* {reservation.u} */}
+                                </p>
+                            </td>
+                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                {reservation.num_tickets}
+                            </td>
+                            <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                {/* <button
+                                    className={`text-white bg-green-500 m-1 p-3 rounded-md  ${
+                                        isDeleting === e.id
+                                            ? "opacity-50 cursor-not-allowed"
+                                            : ""
+                                    }`}
+                                    onClick={() => handleEdit(event.id)}
+                                    disabled={isDeleting === event.id}
+                                >
+                                    {isDeleting === event.id
+                                        ? "Editing..."
+                                        : "Edit"}
+                                </button> */}
+                                {/* <button
+                                    className={`text-white border m-1 p-2 rounded-md  bg-red-500  ${
+                                        isDeleting === event.id
+                                            ? "opacity-50 cursor-not-allowed"
+                                            : ""
+                                    }`}
+                                    onClick={() => handleDelete(event.id)}
+                                    disabled={isDeleting === event.id}
+                                >
+                                    {isDeleting === event.id
+                                        ? "Deleting..."
+                                        : "Delete"}
+                                </button> */}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </AuthenticatedLayout>
     );
 }
